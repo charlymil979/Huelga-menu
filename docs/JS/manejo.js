63e5fe38df4334565c235ada033f2a087f7e8948
Carlos Contreras <86571277+charlymil979@users.noreply.github.com>
2025-01-30 05:02:55 -0300

    Add files via upload

--- docs/JS/manejo.js
+++ docs/JS/manejo.js
@@ -1,6 +1,21 @@
+
 window.addEventListener("DOMContentLoaded", (e) => {
+  // $activas = document.querySelectorAll(".seccion1")
  
   document.addEventListener("click", (e) => {
+    // console.log(e.target)
+    if(e.target.classList.contains("cerrar")){
+      // console.log("cerrar")
+      document.querySelector(".popup").classList.add("invisible");
+    }
+    if(e.target.classList.contains("hh")){
+      document.querySelectorAll(".seccion1").forEach((el) => {
+      el.classList.remove("activa");
+    });
+      console.log("abrir")
+      document.querySelector(".popup").classList.remove("invisible")
+      window.scrollTo(0,0)
+    }
    let $seccion = document.querySelectorAll(`.${e.target.classList[0]}1`);
     $seccion.forEach((el) => {
       if(el != e.target.nextSibling)
@@ -10,6 +25,6 @@ window.addEventListener("DOMContentLoaded", (e) => {
     // console.log(e.target.nextSibling);
     e.target.nextSibling.classList.toggle("activa");
     
-    ;
+    
   });
 });
--- docs/JS/menu.js
+++ docs/JS/menu.js
@@ -1,5 +1,12 @@
 let datos = {};
+/* const url = "https://api.jsonbin.io/v3/b/67848b56e41b4d34e47677a6";
+ mail: carlos.alberto
+const key = "$2a$10$8Qhm0RTQSh3ar9zIVLqApO2sZl.4RLtmnqAfWw9C3EgcDJQP7JiXC"; */
+// const url = "http://localhost:3000/menu";
+
 const url = "https://api.jsonbin.io/v3/b/6480e9ae9d312622a36bfd82";
+
+const key = "$2b$10$7gOb6JjvkSTgwNMBYXHlVO7hXKJZHt4O4vc6RC.YQ7l3QAHg9y7LO"
 
 const $container = document.querySelector(".container");
 
@@ -8,76 +15,103 @@ let $seccion = "",
   $precio,
   $uno,
   clase = "A",
-  art;
+  art,
+  hh = "",
+  arthh = "";
 
 function llamarDb(url) {
-  fetch(url,{headers:{
-    "X-Access-Key":"$2b$10$7gOb6JjvkSTgwNMBYXHlVO7hXKJZHt4O4vc6RC.YQ7l3QAHg9y7LO"}})
+  fetch(url, {
+    headers: {
+      "X-Access-Key": key,
+    },
+  })
     .then((resp) => resp.json())
     .then((dato) => {
-      console.log(dato)
+      // console.log(dato)
       // console.log(dato.record.menu);
-      const data = dato.record;
-      console.log(data)
+      const data = dato.record; //orig = dato.record
+      // console.log(data)
       // Empezando a armar la estructura de datos
       for (const key in data) {
+        const seccionhh = document.createElement("section");
+        seccionhh.classList.add("seccion1");
+        seccionhh.classList.add("visible");
+        let clase = key.replaceAll(" ", "_");
+        seccionhh.classList.add(`${clase}`);
+        const titulohh = document.createElement("h3");
+        titulohh.classList.add("seccion");
+        titulohh.innerHTML = `${key}`;
+
         const seccion = document.createElement("section");
         seccion.classList.add("seccion1");
-        const titulo=document.createElement("h3")
-        titulo.classList.add("seccion")
-        titulo.innerHTML=`${key} HUELGA` ;
-        // console.log(key);
+        const titulo = document.createElement("h3");
+        titulo.classList.add("seccion");
+        titulo.innerHTML = `${key}`;
         if (key != "id") {
-          
+          let clase2 = key.replaceAll(" ", "_");
+          seccion.innerHTML = `<div class= 'pictures'><img src='./imagenes/${clase2}.jpg' alt='${key}'></div>`;
           for (const articulo in data[key]) {
             let $tipos = "";
+            let $tiposhh = "";
             datos = data[key][articulo];
             // console.log(datos);
-            
+
             $precio = "";
 
-           //Armando el arreglo 
-            datos[2].forEach((element,i) => {
-              if(datos[0].includes("INGREDIENTES")){
-                $tipos += `
-                <span class="tipo">${element[0]} - </span>
-                `;
-                if(i>0 && i%3===0){
-                  $tipos += `<br>`
-                }
-              }
-                
-                else{
-              $tipos += `<div>
+            //Armando el arreglo
+            datos[2].forEach((element, i) => {
+              
+                $tipos += `<div>
               <span class="tipo">${element[0]}</span>
-              <span class="precio">$ ${element[1]}</span>
+              <span class="precio white-space: pre;">${element[1]}</span>
               </div>
-              `;}
+              `;
+            
               // console.log(element);
+
+              if (element[2] != "" && element[2] != "undefined") {
+                // console.log(element[2]);
+                $tiposhh += `<div>
+              <span class="tipo">${element[0]}</span>
+              <span class="precio">${element[2]}</span>
+              </div>`;
+              }
             });
             art = `
-            <h4 class="articulo">${datos[0]}</h4><div class="articulo1">
+            <div class="articulo1">
+            <h4 class="articulo">${datos[0]}</h4>
             <div class="descripcion">${datos[1]}</div>
             <div class="tipos">${$tipos}</div>
             </div>
             `;
+            if ($tiposhh != "") {
+              arthh = `
+            <div class="hh1">
+            <h4 class="hhart">${datos[0]}</h4>
+            <div class="hhtipos">${$tiposhh}</div>
+            </div>
+            `;
+              // <div class="descripcion">${datos[1]}</div>
+              seccionhh.innerHTML += arthh;
+              document.querySelector(".modal").appendChild(titulohh);
+              document.querySelector(".modal").appendChild(seccionhh);
+            }
             // console.log($tipos);
             // console.log(datos[0]);
             // console.log(datos[1]);
             // console.log(datos[2][0][0]);
             // console.log(datos[2][0][1]);
             seccion.innerHTML += art;
-          
-          document
-          .querySelector(".menu")
-          .appendChild(titulo);
-          document
-          .querySelector(".menu")
-          .appendChild(seccion);
-        }
+
+            document.querySelector(".menu").appendChild(titulo);
+            document.querySelector(".menu").appendChild(seccion);
+          }
         }
       }
-    });
+      document.querySelector(".menu").innerHTML +=
+        '<BUTTON  class="happy hh"><span class="hh">HAPPY HOUR</span><img class="click" src="./imagenes/click.svg" alt="click"></BUTTON> ';
+    })
+    .then(()=>{window.scrollTo(0,0)});
 }
 
 llamarDb(url);
